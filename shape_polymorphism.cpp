d9d31579047884d461d5af992ca1f16d3252e6d2
Toyse-dev <olayonwatoyib05@gmail.com>
2026-07-30 12:09:17 +0100

    Refactor shape drawing methods and constructors

--- shape_polymorphism.cpp
+++ shape_polymorphism.cpp
@@ -3,35 +3,41 @@ using namespace std;
 
 class Shape {
 	public:
-		virtual void draw() {
-			cout << "Drawing a generic shape" << endl;
-		}
-		virtual ~Shape() {}
+	void virtual draw() {
+		cout << "Drawing a generic shape" << endl;
+	}
+	virtual ~Shape() {}
 };
 
 class Circle: public Shape {
+	private: double radius;
 	public:
+		Circle(double r) : radius(r) {}
+		
 		void draw() override {
-			cout << "Drawing a circle with radius: 10cm" << endl;
+			cout << "Drawing a circle with: " << radius << "cm radius" << endl;
 		}
 };
 
 class Square: public Shape {
+	private: double side;
 	public:
+		Square(double s) : side(s) {}
+		
 		void draw() override {
-			cout << "Drawing a square with 4 side" << endl;
+			cout << "Drawing a square with: " << side << "cm side" << endl;
 		}
 };
 
 int main() {
 	
 	Shape* shape[2];
-	shape[0] = new Circle();
-	shape[1] = new Square();
+	shape[0] = new Circle(10);
+	shape[1] = new Square(4);
 	
 	for (int i = 0; i < 2; i++) {
 		shape[i]->draw();
-	};
+	}
 	
 	delete shape[0];
 	delete shape[1];
