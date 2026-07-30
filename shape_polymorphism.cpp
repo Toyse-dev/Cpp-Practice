1b872e64b3704f6f8ec4558dd9755308bcf4c73e
Toyse-dev <olayonwatoyib05@gmail.com>
2026-07-30 11:17:47 +0100

    Refactor draw methods and add main function for shapes
    Updated draw methods in Circle and Square classes to use fixed values instead of variables. Added main function to demonstrate polymorphism with shape objects.

--- shape_polymorphism.cpp
+++ shape_polymorphism.cpp
@@ -12,15 +12,29 @@ class Shape {
 class Circle: public Shape {
 	public:
 		void draw() override {
-			cout << "Drawing a circle with radius: " << <radius> << endl;
+			cout << "Drawing a circle with radius: 10cm" << endl;
 		}
 };
 
 class Square: public Shape {
 	public:
 		void draw() override {
-			cout << "Drawing a square with sode:" << <side> << endl;
+			cout << "Drawing a square with 4 side" << endl;
 		}
 };
 
-
+int main() {
+	
+	Shape* shape[2];
+	shape[0] = new Circle();
+	shape[1] = new Square();
+	
+	for (int i = 0; i < 2; i++) {
+		shape[i]->draw();
+	};
+	
+	delete shape[0];
+	delete shape[1];
+	
+	return 0;
+}
