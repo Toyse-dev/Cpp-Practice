a8065e9246e753eb2554acd6f5aa2657e3d18f40
Toyse-dev <olayonwatoyib05@gmail.com>
2026-08-03 09:12:14 +0100

    Refactor to use std::string and std::cout

--- payment_abstraction.cpp
+++ payment_abstraction.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 class PaymentMethod {
 	public:
@@ -11,29 +10,29 @@ class PaymentMethod {
 
 class CreditCard : public PaymentMethod {
 	private:
-		string cardNumber;
+		std::string cardNumber;
 	public:
 		
-		CreditCard(string num) : cardNumber(num.substr(num.length() - 4)) {}
+		CreditCard(std::string num) : cardNumber(num.substr(num.length() - 4)) {}
 
 		void pay(double am) override {
-			cout << "Paid $" << am <<" using Credit Card ending in "  << cardNumber << endl;
+			std::cout << "Paid $" << am <<" using Credit Card ending in "  << cardNumber << std::endl;
 		}
 		
 		void printReceipt() override {
-			cout << "Credit Card receipt" << endl;
+			std::cout << "Credit Card receipt" << std::endl;
 		}
 };
 
 class PayPal : public  PaymentMethod {
-	private: string email;
+	private: std::string email;
 	public:
-		PayPal(string e) : email(e) {}
+		PayPal(std::string e) : email(e) {}
 		void pay(double p) override {
-			cout << "Paid $" << p << " using Paypal account " << email << endl;
+			std::cout << "Paid $" << p << " using PayPal account " << email << std::endl;
 		}
 		void printReceipt() override {
-			cout << "PayPal receipt" << endl;
+			std::cout << "PayPal receipt" << std::endl;
 		}
 };
 
@@ -42,7 +41,7 @@ int main() {
 	pm->pay(50);
 	pm->printReceipt();
 	
-	cout << endl;
+	std::cout << std::endl;
 	
 	PaymentMethod* pay = new PayPal("toyse@gmail.com");
 	pay->pay(20);
