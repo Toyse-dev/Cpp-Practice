5a96dc3d57649823b0ab831d272e5385f015b628
Toyse-dev <olayonwatoyib05@gmail.com>
2026-08-23 10:23:01 +0100

    Refactor Credential class and add Vault size method
    Updated password handling to store the full password and display only the last four characters for security. Added a method to get the size of the vault.

--- ToyseVault.cpp
+++ ToyseVault.cpp
@@ -6,14 +6,15 @@ class Credential {
     private:
         std::string site, username, password;
     public:
-        Credential(std::string s, std::string u, std::string p) : site(s), username(u), password(p.substr(p.length() - 4)) {}
+        Credential(std::string s, std::string u, std::string p) : site(s), username(u), password(p) {}
         void display() const {
             std::cout << "Site name: " << site << std::endl;
             std::cout << "Username: " << username << std::endl;
-            if (password.length() <= 8) {
+            if (password.length() < 8) {
                 std::cout << "Password too weak, reset password" << std::endl;
             } else {
-                std::cout << "Password: " << " ****" << password << std::endl;
+                std::string pass = password.substr(password.length() - 4);
+                std::cout << "Password: " << " ****" << pass << std::endl;
             }
         };
         std::string getSite() { return site; }
@@ -29,4 +30,22 @@ class Vault {
         void add(const Credential& c) {
             items.push_back(c);
         }
+
+        size_t size() const {
+            return items.size();
+        }
 };
+
+int main() {
+    Vault myVault;
+
+    Credential cred1("Facebook", "Toyse", "12345678");
+
+    myVault.add(cred1);
+
+    cred1.display();
+
+    std::cout << "Vault size: " << myVault.size() << std::endl;
+
+    return 0;
+}
